6a73274aa112d45dfc4f2b2417b7dcdff1a09240
Vennverse <99286109+Vennverse@users.noreply.github.com>
2025-07-28 17:34:43 +0530

    dc

--- simple-db-fix.sql
+++
@@ -1,4 +0,0 @@
--- Simple database fix SQL
-ALTER USER autojobr_user WITH PASSWORD 'autojobr_2025_secure';
-GRANT ALL PRIVILEGES ON DATABASE autojobr TO autojobr_user;
-GRANT CONNECT ON DATABASE autojobr TO autojobr_user;
