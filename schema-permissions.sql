f084fe43037c648df479dcc0bc420ba141de7f9e
Vennverse <99286109+Vennverse@users.noreply.github.com>
2025-08-12 02:35:49 +0530

    sc

--- schema-permissions.sql
+++
@@ -1,8 +0,0 @@
--- Schema permissions for autojobr database
-GRANT ALL ON SCHEMA public TO autojobr_user;
-GRANT USAGE ON SCHEMA public TO autojobr_user;
-GRANT CREATE ON SCHEMA public TO autojobr_user;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO autojobr_user;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO autojobr_user;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO autojobr_user;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO autojobr_user;
