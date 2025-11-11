4d890aa142c5a1a89efbc8acc3fab9b7939f6cfc
Vennverse <99286109+Vennverse@users.noreply.github.com>
2025-11-12 03:56:00 +0530

    SD

--- server/userRoleService.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class UserRoleService {
-  static assignUserRole(email: string, preferredRole?: string): Promise<{
-    userType: string;
-    currentRole: string;
-  }>;
-  static detectUserRole(email: string): string;
-  static updateUserRole(userId: string, newRole: string): Promise<boolean>;
-}
